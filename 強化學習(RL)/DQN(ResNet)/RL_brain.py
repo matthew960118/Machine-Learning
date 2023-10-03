06227a585943bdcac1ec4d4db6fceda4462bc24c
matthew960118 <93273944+matthew960118@users.noreply.github.com>
2023-10-03 12:06:25 +0800

    added choose_action, learn

--- 強化學習(RL)/DQN(ResNet)/RL_brain.py
+++ 強化學習(RL)/DQN(ResNet)/RL_brain.py
@@ -29,6 +29,7 @@ class RL_brain():
         self.epsilon = 0 if e_gerrdy_increment is not None else self.epsilon_max
         
         self.memory = np.ndarray((self.memory_size,))
+        self.learn_step_counter = 0
         
     def store_memory(self,s,a,r,s_):
         if not hasattr(self, 'memory_counter'):
@@ -39,8 +40,32 @@ class RL_brain():
         self.memory[index, :] = transition
 
         self.memory_counter += 1
+    
     def _build_net_(self):
         self.eval_net = ResNet.resent18(self.n_actions)
         self.target_net = ResNet.resent18(self.n_actions)
+    
+    def choose_action(self, state):
+        state = np.expand_dims(state, axis=0)  # Add an additional dimension to make it (batch_size, n_features)
+        if np.random.uniform() < self.epsilon:
+            q_values = self.eval_net(state)
+            action = tf.argmax(q_values, axis=1).numpy()[0]
+        else:
+            action = np.random.choice(self.n_actions)
+        return action
+    
+    def learn(self):
+        if self.learn_step_counter%self.replace_target_iter:
+            w = self.eval_net.get_weights
+            self.target_net.set_weights(w)
+
+        batch_memory = np.random.choice(self.memory,self.batch_size,replace=False)
         
+        batch_state_ = batch_memory[:,-self.n_features-1:-1]
+        batch_state = batch_memory[:,:self.n_features]
+        batch_reward = batch_memory[:,self.n_features+1]
         
+        with tf.GradientTape as tape:
+            q_eval = self.eval_net(batch_state)
+            q_next = self.target_net(batch_state_)
+            
